1df510f29020c86bac1b035088ddc44d79e381f1
Dave Ryan Sanz <dave.ryan.r.sanz@accenture.com>
2025-08-13 08:05:48 +0000

    Add Save/Clear button and functions

--- webapp/controller/Act1View.controller.js
+++ webapp/controller/Act1View.controller.js
@@ -5,6 +5,26 @@ sap.ui.define([
 
     return Controller.extend("my.sapui5.act1.training.act1simpleform.controller.Act1View", {
         onInit() {
+        },
+
+        onSave: function(){
+            alert("Data is saved successfully.");
+        },
+
+        onClear: function(){
+            // Get Form View
+            var oForm = this.getView().byId("simpleForm");
+            // Point for form content
+            var oContent = oForm.getContent();
+            {
+                // Loop at each field in the form, then set the value to null/space
+                oContent.forEach( function(oControl){
+                    if(oControl instanceof sap.m.Input || oControl instanceof sap.m.Select)
+                {
+                    oControl.setValue("");
+                }
+                })    
+            }
         }
     });
 });
